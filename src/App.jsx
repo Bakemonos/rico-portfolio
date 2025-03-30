3b6124c97ea3c9c11c0488264b4c1fe7f02a73d7
Bakemono <149962076+Bakemonos@users.noreply.github.com>
2025-03-30 13:15:53 +0800

    Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './Components/navigation_bar'
+import Hero from './Components/hero'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/hero.jsx
@@ -0,0 +1,22 @@
+export default function Hero(){
+    return(
+        <>
+            <div className="Hero">
+                <section className="welc">
+                    <h1>Hello, I'm a Front end Developer</h1>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and 
+                        typesetting industry. Lorem Ipsum has been the industry's
+                        standard dummy text ever since the 1500s,
+                    </p>
+                    <button>
+                        Creator Journey
+                    </button>
+                </section>
+                <section>
+
+                </section>
+            </div>
+        </>
+    )
+}
